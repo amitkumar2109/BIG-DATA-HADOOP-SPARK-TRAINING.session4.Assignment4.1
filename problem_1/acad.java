8d40c8cbf2e60d883c98bdf1f2c584828879b543
amitkumar2109 <amitkumar210986@gmail.com>
2017-11-20 00:16:57 +0530

    problem done

---
+++ problem_1/acad.java
@@ -0,0 +1,17 @@
+/*
+ *  Sum  of two int
+ */
+package acadGild_Project;
+
+public class acad {
+	
+	public static void main(String args[]){
+		// hard coded the two int value
+		int var1= 5;
+		int var2=10;
+		
+		int var3 = var1+var2;
+		System.out.print("Addition of two integer:"+var3);			
+		
+	}
+}
